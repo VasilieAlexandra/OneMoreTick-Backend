d7324cf484810f2a6e4f4c4ed0fa9cd08062514c
VasilieAlexandra <alexandra.vasilie@student.unitbv.ro>
2022-12-28 14:25:29 +0200

    Created base project

---
+++ src/main/java/com/app/onemoretick/OnemoretickApplication.java
@@ -0,0 +1,13 @@
+package com.app.onemoretick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnemoretickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnemoretickApplication.class, args);
+	}
+
+}
